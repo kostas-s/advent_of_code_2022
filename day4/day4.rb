d7b446ee0f70d4d69784766379906fff6bd51a17
Konstantinos Smonos <65230255+kostas-s@users.noreply.github.com>
2022-12-04 10:13:22 +0200

    day 4b ruby

--- day4/day4.rb
+++ day4/day4.rb
@@ -1,7 +1,20 @@
 lines = File.readlines('day4.txt').map(&:chomp).map { |l| l.split(',') }
-puts(lines.filter do |l|
+
+def line_to_range_arrays(l)
   first = l[0].split('-')
   second = l[1].split('-')
-  common = (first[0]..first[1]).to_a & (second[0]..second[1]).to_a
-  common == (first[0]..first[1]).to_a || common == (second[0]..second[1]).to_a
-end.count)
+  [(first[0]..first[1]).to_a, (second[0]..second[1]).to_a]
+end
+
+# Part A
+puts("Part A: #{lines.filter do |l|
+  ranges = line_to_range_arrays(l)
+  common = ranges[0] & ranges[1]
+  common == ranges[0] || common == ranges[1]
+end.count}")
+
+# Part B
+puts("Part B: #{lines.filter do |l|
+  ranges = line_to_range_arrays(l)
+  (ranges[0] & ranges[1]).any?
+end.count}")
