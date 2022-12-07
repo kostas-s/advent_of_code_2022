56e0714c95fc871520eb0f2a34d2ce7c0c3a4f3e
Konstantinos Smonos <65230255+kostas-s@users.noreply.github.com>
2022-12-07 14:25:35 +0200

    day 7b

--- day7/day7.rb
+++ day7/day7.rb
@@ -2,4 +2,9 @@ require './elf_file_system'
 
 file_lines = File.readlines('input.txt')
 fs = ElfFileSystem.new(file_lines)
-puts("Part A: #{fs.directories.filter { |d| d.total_size <= 100000 }.map(&:total_size).sum(0)}")
+puts("Part A: #{fs.directories.filter { |d| d.total_size <= 100_000 }.map(&:total_size).sum(0)}")
+
+free_space = 70_000_000 - fs.directories.first.total_size
+missing_space = 30_000_000 - free_space
+dir_to_del = fs.directories.filter { |d| d.total_size >= missing_space }.min_by(&:total_size)
+puts("Part B: #{dir_to_del}")
